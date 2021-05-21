f242f37bf552f7a7bfc05acd7c93e1f935eae014
stephanie-p8 <72699488+stephanie-p8@users.noreply.github.com>
2021-05-21 09:09:26 +0200

    import navigation

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import SamplerEditionNavigation from "./components/navigation/Navigation";
+import AddSampler from "./components/sampler/addSampler";
 
+const Tabs = createBottomTabNavigator();
 
-export default function App() {
+const App = () => {
   
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            switch (route.name) {
+              case "Sampler":
+                iconName = focused ? "musical-notes" : "musical-notes-outline";
+                break;
+              case "Add sampler":
+                iconName = focused ? "add-circle" : "add-circle-outline";
+                break;
+              default:
+                iconName = "ban";
+                break;
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{ activeTintColor: "tomato", inactiveTintColor: "gray" }}
+      >
+
+        <Tabs.Screen name="Sampler" component={SamplerEditionNavigation}/>
+        <Tabs.Screen name="Add sampler">
+          {(props) => <AddSampler {...props} />}
+        </Tabs.Screen>
+        
+      </Tabs.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -21,3 +52,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
